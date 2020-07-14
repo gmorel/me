941d6f4691630434a9f1796fa1af08322ccdbf83
Guillaume MOREL <me@gmorel.io>
2020-07-14 17:23:34 +0200

    Introduce Gallery
    Sometimes pictures convey more meaning than sentences.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {GalleryComponent} from "./gallery/gallery.component";
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'more', component: GalleryComponent },
 ];
 
 @NgModule({
